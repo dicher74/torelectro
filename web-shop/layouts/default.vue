d3e1086f121fcf14456baf75f97b79893d163fe4
dicher74 <dicher74@gmail.com>
2024-10-22 13:23:19 +0300

    Задана основная архитектура

---
+++ web-shop/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="content-wrapper">
+		<Nuxt />
+	</div>
+</template>
+
+<style scoped>
+.content-wrapper {
+	background: #F0F0F0;
+	padding: 20px 50px 0;
+	margin: 0;
+	height: 100vh;
+}
+</style>
---
+++ web-shop/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<DefaultHeader />
+	<PageMainInformation />
+	<ProductCardsArea />
+	<PresentationBanner />
+	<CompanyInformation />
+	<ConsultationBanner />
+	<PopularQuestionsArea />
+	<InterestingNuansionsArea />
+	<DefaultFooter />
+</template>
+
+<script>
+import DefaultFooter from '~/components/Footers/DefaultFooter/DefaultFooter.vue';
+import DefaultHeader from '~/components/Headers/DefaultHeader/DefaultHeader.vue';
+import CompanyInformation from '~/components/index-page/CompanyInformation/CompanyInformation.vue';
+import ConsultationBanner from '~/components/index-page/ConsultationBanner/ConsultationBanner.vue';
+import InterestingNuansionsArea from '~/components/index-page/InterestingNuansionsArea/InterestingNuansionsArea.vue';
+import PopularQuestionsArea from '~/components/index-page/PopularQuestionsArea/PopularQuestionsArea.vue';
+import PresentationBanner from '~/components/index-page/PresentationBanner/PresentationBanner.vue';
+import ProductCardsArea from '~/components/index-page/ProductCardsArea/ProductCardsArea.vue';
+
+export default {
+	layout: "default",
+}
+</script>
