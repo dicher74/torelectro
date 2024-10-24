fcbe4fa4fc28ea70d0ea16c3acc97e5a733e33b9
dicher74 <dicher74@gmail.com>
2024-10-24 20:09:04 +0300

    создан диалог вакансии

---
+++ web-shop/components/Dialogs/VacansionDialog/VacansionDescription.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="vacansion-description">
+		<div 
+			v-for="(block, blockNum) in blockList" :key="`block_num-${blockNum}`" 
+			v-html="block"
+			@click="check(block)"
+			class="vacansion-description__block"> 
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			blockList: [
+				`<span style='color: #FF3D00;'>Профессиональная команда</span>,
+				обеспечивающая цикл производства,
+				сначала и до самого конца`,
+
+				`<span style='color: #FF3D00;'>Грамотная подготовка</span>, предоставляющая
+				понятное введение в должность `,
+
+				`<span style='color: #FF3D00;'>Понятная работа</span>, с официальным
+				трудоустройством и всеми социальными, 
+				корпоративными гарантиями`,
+
+				`<span style='color: #FF3D00;'>Карьерная лестница</span>, c понятным ростом 
+				и необходимыми корпоративными, 
+				производственными обязанностями`
+			]
+		}
+	},
+	methods: {
+		check(block) {
+			console.log(block)
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.vacansion-description {
+	display: inline-flex;
+	flex-direction: column;
+	gap: 10px;
+	width: 360px;
+	align-items: stretch;
+
+	&__block {
+		white-space: pre-line;
+		background-color: rgba(245, 245, 245, 1);
+		border-radius: 5px;
+		padding: 10px;
+		font-size: 16px;
+		line-height: 17.6px;
+	} 
+}
+</style>
---
+++ web-shop/components/Dialogs/VacansionDialog/VacansionDialog.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="vacansion-dialog">
+		<div class="vacansion-dialog__title">Вакансия для амбициозных, сильных и коммуникабельных</div>
+		<div class="vacansion-dialog__body">
+			<div class="dialog-body__right">
+				<VacansionDescription />
+				<ButtonsOrangeButton :width="360" :height="60" text="Анкета для заполнения"/>
+				<div class="button-area__policy-text policy-text">
+					<p class="policy-text__par_top"> нажимая, вы соглашаетесь </p>
+					<a class="policy-text__policy-link"> с условиями политики конфиденциальности </a>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import VacansionDescription from './VacansionDescription.vue';
+
+export default {
+	components: {
+		VacansionDescription,
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.vacansion-dialog {
+	width: 100%;
+	display: inline-flex;
+	flex-direction: column;
+	gap: 20px;
+
+	&__title {
+		height: 50px;
+		white-space: nowrap;
+		font-size: 24px;
+		line-height: 26.4px;
+		display: inline-flex;
+		align-items: center;
+	}
+
+	&__body {
+		display: inline-flex;
+		justify-content: flex-end;
+	}
+}
+
+.dialog-body__right {
+	display: inline-flex;
+	flex-direction: column;
+	gap: 20px;
+}
+
+.policy-text {
+	display: inline-flex;
+	flex-direction: column;
+	text-align: center;
+	font-size: 14px;
+
+	&__par_top {
+		color: #909090;
+		line-height: 15.4px;
+	}
+	&__policy-link {
+		color: #3366BB;
+		text-decoration: underline;
+		line-height: 15.4px;
+	}
+}
+</style>
