26d03b5ca6931d28c03c1cc1571b8aeea0155858
dicher74 <dicher74@gmail.com>
2024-10-26 18:24:02 +0300

    диалоги по центру

--- web-shop/components/Dialogs/DialogsArea.vue
+++ web-shop/components/Dialogs/DialogsArea.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="dialog-area">
 		<div class="dialog__shadow">
-			<div class="dialogs-container">
-				<Dialog 
+			<div class="dialogs-container" :style="`margin-top: ${marginTop}px`">
+				<Dialog
 					v-for="(dialog, dialogNum) in dialogList" :key="`diallog_num-${dialogNum}`"
 					:dialogMode="dialog" />
 			</div>
@@ -18,14 +18,42 @@ export default {
 	components: {
 		Dialog,
 	},
+	data() {
+		return {
+			firstDialogHeight: 0
+		}
+	},
 	computed: {
 		dialogList() {
 			return store.state.dialogList
+		},
+		central() {
+			const centralModes = ['presentation', 'consultation', 'product']
+			for (const dialog of this.dialogList) {
+				if (centralModes.includes(dialog)) {
+					return true;
+				}
+			}
+			return false;
+		},
+		marginTop() {
+			if (this.central) {
+				console.log(window.scrollY)
+				console.log('page height:', window.innerHeight)
+				return window.scrollY - this.firstDialogHeight / 2 + window.innerHeight / 2 - 110
+			}
+			return 0;
+		},
+		styleList() {
+			return `margin-top: ${this.marginTop - 90}px;`
 		}
 	},
 	mounted() {
-		console.log('show dialog!')
-		store.commit('scrollToTop')
+		if (!this.central) {
+			store.commit('scrollToTop')
+		}
+		this.firstDialogHeight = document.querySelector('.dialog__window').getBoundingClientRect().height
+		console.log(this.firstDialogHeight)
 	}
 }
 </script>
